a30bdfb4a936e54de14c881fe10433fbc064eb1e
NyawiraMuturi <albinamuturi@gmail.com>
2026-02-19 23:50:47 +0300

    chore(responsiveness): make pages more responsive

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
   }
 
   return (
-    <div className="max-h-screen bg-[url('/hero.jpg')] bg-cover bg-center overflow-hidden">
+    <div className="max-h-screen bg-[url('/hero.jpg')] bg-cover bg-center overflow-hidden bg-gradient-to-t from-black/80 to-transparent">
       <main className="h-screen relative">
         <div className="flex justify-end p-4 space-x-4">
           <Button onClick={openLogin}>Login</Button>
@@ -34,8 +34,15 @@ export default function Home() {
           alt="Hero Image"
           width={550}
           height={600}
-          className="absolute inset-0 mx-auto"
+          className="absolute inset-0 mx-auto z-10 hidden md:block"
         />
+
+        <section>
+          <div className="absolute bottom-[5%] left-[45%] right-0 p-8 text-center border-2 border-[#709110] w-[30%] bg-[#baf11b] rounded-md">
+            <h2 className="text-3xl font-bold text-black mb-4">Track Your Challenges, Achieve Your Goals</h2>
+            <p className="text-lg font-light mb-6">Join Juhudi today and start your journey towards personal growth and achievement.</p>
+          </div>
+        </section>
       </main>
 
       <AuthDialog
